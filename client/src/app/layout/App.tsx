5dbdd0dfaf334601f7bf018a29e0a655e12250bc
yonatan goldin <yonatangoldin69@gmail.com>
2023-01-21 21:18:53 +0200

    header, links and routes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
     const [darkMode, setDarkMode] = useState(false);
@@ -13,7 +18,7 @@ function App() {
         palette: {
             mode: paletteType,
             background: {
-                default: paletteType === "light" ? "#eaeaea" : "#121212",
+                default: paletteType === "light" ? "#e0e0e0" : "#424242",
             },
         },
     });
@@ -30,8 +35,12 @@ function App() {
                     darkMode={darkMode}
                     handleThemeChange={handleThemeChange}
                 />
-                <Container>
-                    <Catalog />
+                <Container sx={{ mb: 4 }}>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/catalog" component={Catalog} />
+                    <Route path="/catalog/:id" component={ProductDetails} />
+                    <Route path="/about" component={AboutPage} />
+                    <Route path="/contact" component={ContactPage} />
                 </Container>
             </ThemeProvider>
         </>
