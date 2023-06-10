4df96bc55299aa82381d56a42503fed7fd681964
yonatan goldin <yonatangoldin69@gmail.com>
2023-06-10 17:49:08 +0300

    refactored error handling to work w redux

--- client/src/features/catalog/catalogSlice.ts
+++ client/src/features/catalog/catalogSlice.ts
@@ -9,13 +9,25 @@ import { RootState } from "../../app/store/configureStore";
 
 const productsAdapter = createEntityAdapter<Product>();
 
+// getting all products from DB and storing them in the redux store
 export const fetchProductsAsync = createAsyncThunk<Product[]>(
     "catalog/fetchProductsAsync",
-    async () => {
+    async (_, thunkAPI) => {
         try {
             return await agent.Catalog.list();
-        } catch (error) {
-            console.log(error);
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
+        }
+    }
+);
+
+export const fetchProductAsync = createAsyncThunk<Product, number>(
+    "catalog/fetchProductAsync",
+    async (productId, thunkAPI) => {
+        try {
+            return await agent.Catalog.details(productId);
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue({ error: error.data });
         }
     }
 );
@@ -39,7 +51,22 @@ export const catalogSlice = createSlice({
             state.productsLoaded = true;
         });
 
-        builder.addCase(fetchProductsAsync.rejected, (state) => {
+        builder.addCase(fetchProductsAsync.rejected, (state, action) => {
+            console.log(action.payload);
+            state.status = "idle";
+        });
+
+        builder.addCase(fetchProductAsync.pending, (state) => {
+            state.status = "pendingFetchProduct";
+        });
+
+        builder.addCase(fetchProductAsync.fulfilled, (state, action) => {
+            productsAdapter.upsertOne(state, action.payload);
+            state.status = "idle";
+        });
+
+        builder.addCase(fetchProductAsync.rejected, (state, action) => {
+            console.log(action);
             state.status = "idle";
         });
     },
